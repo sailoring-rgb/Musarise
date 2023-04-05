3162d0c8453b41a713c5260b0151e1600fc3b5fc
Ana Henriques <pbapohenriques@hotmail.com>
2023-04-05 22:07:15 +0100

    Initial Commit

---
+++ teste/testeApp.swift
@@ -0,0 +1,17 @@
+//
+//  testeApp.swift
+//  teste
+//
+//  Created by annaphens on 05/04/2023.
+//
+
+import SwiftUI
+
+@main
+struct testeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
