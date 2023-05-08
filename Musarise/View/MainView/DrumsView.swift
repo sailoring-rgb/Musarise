699fa2b8e5df7cf183fd0127295f55a30a65286b
Ana Henriques <pbapohenriques@hotmail.com>
2023-05-08 18:48:09 +0100

    drums sound recorder

--- Musarise/View/MainView/DrumsView.swift
+++ Musarise/View/MainView/DrumsView.swift
@@ -10,11 +10,6 @@ struct DrumsView: View {
     @State private var showModal : Bool = false
     @State private var audioSelected : URL?
     
-    @StateObject var screenRecorder = ScreenRecorder()
-    @State private var isRecording = false
-    @State private var recorded = false
-    @State private var timer: Timer?
-    
     var body: some View {
         ZStack {
             NavigationView{
@@ -60,7 +55,7 @@ struct DrumsView: View {
 
             if showModal{
                 Color.black
-                    .opacity(0.4) // Adjust the transparency here
+                    .opacity(0.4)
                     .edgesIgnoringSafeArea(.all)
                     .onTapGesture {
                         self.showModal = false
@@ -100,8 +95,150 @@ struct DrumsView: View {
     }
 }
 
-struct DrumsView_Previews: PreviewProvider {
-    static var previews: some View {
-        DrumsView()
+struct PlayCardView: View {
+    let motionManager = CMMotionManager()
+    var onClose: () -> Void
+    
+    @State private var accelerationData: CMAcceleration? = nil
+    @State private var isDetected: Bool = false
+    @State private var startime: Date?
+    @State private var systemNameCancel = "xmark.circle"
+    @State private var systemNameDone = "checkmark.circle"
+    
+    @StateObject var screenRecorder = ScreenRecorder()
+    @State private var isRecording = false
+    @State private var recorded = false
+    @State private var confirmSave = false
+    @State private var elapsedTime: TimeInterval = 0
+    @State private var timer: Timer?
+    
+    @State var players: [AVPlayer] = []
+    @State var audioURL: URL
+    
+    var body: some View {
+        VStack{
+            if let acceleration = accelerationData {
+                VStack{
+                    Circle()
+                        .fill(Color.yellow)
+                        .frame(width: 150, height: 150)
+                        .overlay(
+                            VStack{
+                                Text("Record")
+                                    .foregroundColor(.white)
+                                    .font(.system(size: 25))
+                                
+                                Text("\(elapsedTime, specifier: "%.1f")s")
+                                    .font(.headline)
+                                    .foregroundColor(.white)
+                                    .padding(.top, 10)
+                            }
+                                .padding(.bottom, 5)
+                                .padding(.top, 15)
+                        )
+                        .onTapGesture {
+                            if isRecording {
+                                self.isRecording = false
+                                self.timer?.invalidate()
+                                self.recorded = true
+                                screenRecorder.stopRecording()
+                            } else {
+                                self.isRecording = true
+                                self.recorded = false
+                                self.elapsedTime = 0
+                                screenRecorder.startRecording(mic:true)
+                                self.timer = Timer.scheduledTimer(withTimeInterval: 0.1, repeats: true) { timer in
+                                    if self.isRecording {
+                                        self.elapsedTime += 0.1
+                                    } else {
+                                        timer.invalidate()
+                                    }
+                                }
+                            }
+                        }
+                    
+                    Button(action: {
+                        print("let's save!")
+                        self.confirmSave = true
+                    }) {
+                        Text("Save")
+                    }
+                    .disabled(!recorded)
+                }
+                
+            } else {
+                ProgressView()
+            }
+        }
+        .sheet(isPresented: $confirmSave) {
+            SaveSoundForm(instrument: "Drums", instrumentIcon: "🥁", recorded: $recorded, confirmSave: $confirmSave)
+        }
+        .frame(width: UIScreen.main.bounds.size.width / 1.3, height: UIScreen.main.bounds.size.height / 3)
+        .padding(.bottom, 10)
+        .padding(.top, 30)
+        .background(isRecording ? Color.yellow : Color.white)
+        .cornerRadius(20)
+        .onAppear {
+            startAccelerometerUpdates()
+        }
+        .onDisappear {
+            stopAccelerometerUpdates()
+            players.forEach { $0.pause() }
+            players.removeAll()
+        }
+    }
+    
+    func playSound(audioURL: URL, volume: Float){
+        let playerItem: AVPlayerItem = AVPlayerItem(url:audioURL)
+        let player = AVPlayer(playerItem: playerItem)
+        self.players.append(player)
+        player.volume = volume
+        player.play()
+    }
+
+    func getAmplitude(duration: TimeInterval) -> Double{
+        let min_duration = 1.0
+        let max_duration = 10.0
+        
+        let volume = 1 - ((duration - min_duration) / (max_duration - min_duration))
+        return volume
+    }
+    
+    func checkAcceleration(acceleration: CMAcceleration) {
+        
+        // aceleração inicialmente positiva
+        if (acceleration.y >= 0 && !isDetected) {
+            isDetected = false
+        }
+        // aceleração positiva para negativa
+        else if (acceleration.y < 0 && !isDetected) {
+            isDetected = true
+            startime = Date()
+        }
+        // aceleração negativa para positiva
+        else if (acceleration.y >= 0 && isDetected){
+            if let startime = startime{
+                let duration = Date().timeIntervalSince(startime)
+                let volume = getAmplitude(duration: duration)
+                playSound(audioURL: audioURL, volume: Float(volume))
+            }
+            isDetected = false
+        }
+    }
+
+    func startAccelerometerUpdates(){
+        if motionManager.isAccelerometerAvailable {
+            motionManager.accelerometerUpdateInterval = 0.001
+            motionManager.startAccelerometerUpdates(to: .main) { data, error in
+                if let acceleration = data?.acceleration {
+                    accelerationData = acceleration
+                    checkAcceleration(acceleration: acceleration)
+                }
+            }
+        }
+    }
+
+    func stopAccelerometerUpdates(){
+        motionManager.stopAccelerometerUpdates()
     }
 }
