c3f3a4b04fa42c899607d8ee542d6e961bbca172
parola <parola@Mac.local>
2023-05-10 16:44:51 -0700

    fix: fix Comment title position

--- Musarise/View/MainView/CommentsView.swift
+++ Musarise/View/MainView/CommentsView.swift
@@ -12,71 +12,69 @@ struct CommentsView: View {
     @State var isFetching: Bool = false
     
     var body: some View {
-        NavigationView {
-            VStack {
-                ScrollView {
-                    if isFetching{
-                        ProgressView()
-                            .padding(.top, 20)
-                    } else {
-                        if let post = post{
-                            if let comments = post.comments{
-                                ForEach(comments, id: \.self) { (comment:Comment) in
-                                    HStack {
-                                        WebImage(url: comment.userIconUrl)
-                                            .resizable()
-                                            .aspectRatio(contentMode: .fill)
-                                            .frame(width: 35, height: 35)
-                                            .clipShape(Circle())
+        VStack {
+            ScrollView {
+                if isFetching{
+                    ProgressView()
+                        .padding(.top, 20)
+                } else {
+                    if let post = post{
+                        if let comments = post.comments{
+                            ForEach(comments, id: \.self) { (comment:Comment) in
+                                HStack {
+                                    WebImage(url: comment.userIconUrl)
+                                        .resizable()
+                                        .aspectRatio(contentMode: .fill)
+                                        .frame(width: 35, height: 35)
+                                        .clipShape(Circle())
+                                    
+                                    VStack(alignment: .leading, spacing: 2) {
+                                        Text(comment.userName)
+                                            .fontWeight(.semibold)
                                         
-                                        VStack(alignment: .leading, spacing: 2) {
-                                            Text(comment.userName)
-                                                .fontWeight(.semibold)
-                                            
-                                            Text(comment.text)
-                                            
-                                            Text(comment.publishedDate.formatted(date: .numeric, time: .shortened))
-                                                .font(.caption)
-                                                .foregroundColor(.gray)
-                                        }
+                                        Text(comment.text)
+                                        
+                                        Text(comment.publishedDate.formatted(date: .numeric, time: .shortened))
+                                            .font(.caption)
+                                            .foregroundColor(.gray)
                                     }
-                                    .frame(width: UIScreen.main.bounds.width - 25,alignment: .topLeading)  
-                                    .padding(.horizontal, 16)
-                                    .padding(.vertical, 8)
                                 }
+                                .frame(width: UIScreen.main.bounds.width - 25,alignment: .topLeading)
+                                .padding(.horizontal, 16)
+                                .padding(.vertical, 8)
                             }
                         }
                     }
                 }
-                .refreshable {
-                    isFetching = true
-                    await fetchPost()
-                }
-                HStack {
-                    WebImage(url: userIconURL)
-                        .resizable()
-                        .aspectRatio(contentMode: .fill)
-                        .frame(width: 35, height: 35)
-                        .clipShape(Circle())
-                        .padding(.leading,8)
-                    TextField("Add a comment...", text: $newCommentText)
-                        .textFieldStyle(RoundedBorderTextFieldStyle())
-                        .padding(4)
-                    Button(action: postComment) {
-                        Text("Post")
-                            .fontWeight(.semibold)
-                            .padding(8)
-                    }
-                    .padding(.leading,8)
-                    .foregroundColor(.white)
-                    .background(Color.yellow)
-                    .cornerRadius(15)
-                }
-                .background(Color.gray.opacity(0.1))
             }
-            .navigationBarTitle("Comments")
-            .navigationBarTitleDisplayMode(.inline)
+            .refreshable {
+                isFetching = true
+                await fetchPost()
+            }
+            HStack {
+                WebImage(url: userIconURL)
+                    .resizable()
+                    .aspectRatio(contentMode: .fill)
+                    .frame(width: 35, height: 35)
+                    .clipShape(Circle())
+                    .padding(.leading,8)
+                TextField("Add a comment...", text: $newCommentText)
+                    .textFieldStyle(RoundedBorderTextFieldStyle())
+                    .padding(4)
+                Button(action: postComment) {
+                    Text("Post")
+                        .fontWeight(.semibold)
+                        .padding(8)
+                }
+                .padding(.leading,8)
+                .foregroundColor(.white)
+                .background(Color.yellow)
+                .cornerRadius(15)
+            }
+            .background(Color.gray.opacity(0.1))
         }
+        .navigationBarTitle("Comments")
+        .navigationBarTitleDisplayMode(.inline)
     }
     
     func postComment(){
